cdbbe9aa903a4484cfa5677ea873b72d2c2ef92c
KarlaMonterrubio <20688342+KarlaMonterrubioG@users.noreply.github.com>
2022-12-07 10:50:34 -0600

    Update examples.sh

--- Docker/examples.sh
+++ Docker/examples.sh
@@ -2,10 +2,10 @@
 # Build image for local testing
 # Replace linux/arm64 with linux/amd64 if on a x86_64 system (Not Apple Silicon)
 # Replace 0-9 based on number of cpus on your system. e.g 0-7 for 8 cores.  
-docker buildx build . -t ghcr.io/karlamonterrubiog/competing_risks:latest --platform linux/arm64 --cpuset-cpus 0-9 --load
+docker buildx build . -t ghcr.io/karlamonterrubiog/comprisksvignettes:latest --platform linux/arm64 --cpuset-cpus 0-9 --load
 
 # Build ARM and x86 images and push to GitHub Container Registry
-docker buildx build . -t ghcr.io/karlamonterrubiog/competing_risks:latest --platform linux/arm64,linux/amd64 --cpuset-cpus 0-9 --push
+docker buildx build . -t ghcr.io/karlamonterrubiog/comprisksvignettes:latest --platform linux/arm64,linux/amd64 --cpuset-cpus 0-9 --push
 
 # Use image to render out html files
 # Should pull image from Github Container Registry if not available locally
@@ -13,7 +13,7 @@ docker container run \
   --mount type=bind,source="$(pwd)"/docs,target=/docs \
   --mount type=bind,source="$(pwd)"/Source,target=/Source \
   --mount type=bind,source="$(pwd)"/Data,target=/Data \
-  ghcr.io/karlamonterrubiog/competing_risks ./render
+  ghcr.io/karlamonterrubiog/comprisksvignettes ./render
 
 open docs/CS_specification.html
 
@@ -24,4 +24,4 @@ docker container run \
   --mount type=bind,source="$(pwd)"/Data,target=/Data \
   -e PASSWORD=password \
   -p 8787:8787 \
-  ghcr.io/karlamonterrubiog/competing_risks
+  ghcr.io/karlamonterrubiog/comprisksvignettes
